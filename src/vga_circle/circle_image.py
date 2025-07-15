5b39b54373230671debdd75d2efecf4b7c01b1fc
songhaewon <annto02142374@gmail.com>
2025-07-15 18:05:00 +0900

    edit circle_image more small

--- src/vga_circle/circle_image.py
+++ src/vga_circle/circle_image.py
@@ -1,22 +1,34 @@
 import numpy as np
 
-size = 146
-center = size // 2  # 73
+width = 206
+height = 150
+center_x = width // 2
+center_y = height // 2
 radius = 60
 
 with open('circle_image.coe', 'w') as f:
-    f.write('memory_initialization_radix=2;\n')
+    f.write('memory_initialization_radix=16;\n')
     f.write('memory_initialization_vector=\n')
     
-    for y in range(size):
-        for x in range(size):
-            distance = np.sqrt((x - center)**2 + (y - center)**2)
-            pixel = '1' if distance <= radius else '0'
+    for y in range(height):
+        for x in range(width):
+            distance = np.sqrt((x - center_x)**2 + (y - center_y)**2)
             
-            if y == size-1 and x == size-1:
-                f.write(f'{pixel};')
+            # 12-bit RGB (4-bit R, 4-bit G, 4-bit B)
+            if distance <= radius:
+                # White pixel (FFF in hex)
+                pixel_value = 0xFFF
             else:
-                f.write(f'{pixel},')
+                # Black pixel (000 in hex)
+                pixel_value = 0x000
             
-            if x == size-1:
+            # Format as a 3-character hex string
+            hex_pixel = f'{pixel_value:03X}'
+            
+            if y == height-1 and x == width-1:
+                f.write(f'{hex_pixel};')
+            else:
+                f.write(f'{hex_pixel},')
+            
+            if x == width-1:
                 f.write('\n')
